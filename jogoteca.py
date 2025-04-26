517edc38f27848217e10bc510374c5a42a973e8a
Eduardo Damasceno <eduardo@talentacademy.com.br>
2025-04-26 11:04:47 -0300

    Lesson #2

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/home')
 def ola():
-    return render_template('lista.html')
+    game1 = Game('Tetris', 'Puzzle', 'Atari')
+    game2 = Game('Skyrim', 'RPG', 'Xbox')
+    game3 = Game('Crash', 'Racing', 'PS2')
+
+    gameList = [game1, game2, game3]
+     
+    return render_template('list.html', title='Games', games = gameList)
 
 app.run()
